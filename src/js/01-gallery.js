c32dba20056b60a23df787bfb425cdb6e15df54c
AdrianG24 <adriangolodisin@gmail.com>
2022-09-30 17:57:13 +0300

    first task finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import * as test from './customFunctions/functionGallery';
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import * as test from './customFunctions/functionGallery';
 // Change code below this line
 console.log(test);
